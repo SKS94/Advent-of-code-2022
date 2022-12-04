28d6c08fce2296b69e820665a87fb03644ba1c1f
SKS94 <skste@dtu.dk>
2022-12-05 00:47:49 +0100

    first of january solution

---
+++ advent_of_code_dec_01.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# First puzzle
+max_calorie_elf, temp_calorie_count = 0, 0
+f = open("advent_of_code_dec_01.txt")
+for line in f:
+    if line != "\n":
+        temp_calorie_count += int(line)
+    else:
+        max_calorie_elf = temp_calorie_count if max_calorie_elf < temp_calorie_count else max_calorie_elf
+        temp_calorie_count = 0        
+f.close()
+print(f"The maximum calories carried by one elf is: {max_calorie_elf}")
+
+
+# Second puzzle
+max_calorie_elf, temp_calorie_count = [0, 0, 0], 0
+f = open("advent_of_code_dec_01.txt")
+for line in f:
+    if line != "\n":
+        temp_calorie_count += int(line)
+    else:
+        # As long as the list is sorted only last list item has to checked.
+        max_calorie_elf[-1] = temp_calorie_count if max_calorie_elf[-1] < temp_calorie_count else max_calorie_elf[-1]
+        max_calorie_elf.sort(reverse=True)
+        temp_calorie_count = 0        
+f.close()
+
+print(f"The maximum calories carried by the top three elves are: {max_calorie_elf}")
+print(f"Sum of the above: {sum(max_calorie_elf)}")
